36dada0f49b10c1f93ff3a2cb19a4581642d1a9d
pixelhamza <nashithwk25@gmail.com>
2026-07-13 11:24:42 +0530

    initialize project structure

---
+++ scratch_test.py
@@ -0,0 +1,15 @@
+#this is just an example to chunk
+
+def top_level_function(x):
+    #A module-level function
+    return x + 1
+
+
+class Client:
+#a class
+
+    def request(self, url):
+        return url
+
+    async def aclose(self):
+        pass
